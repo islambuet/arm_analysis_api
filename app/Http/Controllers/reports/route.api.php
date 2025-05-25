fb81d11a5c58df2a51147b561ad78bb6665f5dff
Shaiful Islam <shaiful.islam@aclusterllc.com>
2025-05-25 11:36:20 +0600

    Sales Report Included

--- app/Http/Controllers/reports/route.api.php
+++ app/Http/Controllers/reports/route.api.php
@@ -9,35 +9,11 @@ $controllerClass= Controllers\reports\AnalysisReportController::class;
 Route::middleware('logged-user')->group(function()use ($url,$controllerClass){
     Route::match(['GET','POST'],$url.'/initialize', [$controllerClass, 'initialize']);
     Route::match(['GET','POST'],$url.'/get-items', [$controllerClass, 'getItems']);
-    Route::match(['GET','POST'],$url.'/get-item/{itemId}', [$controllerClass, 'getItem']);
-    Route::post($url.'/save-item', [$controllerClass, 'saveItem']);
 });
-
-
-//bellow code not used
-
-
-
-
-
-$url='reports/varieties';
-$controllerClass= Controllers\reports\VarietiesController::class;
+$url='reports/sales';
+$controllerClass= Controllers\reports\SalesReportController::class;
 /** @noinspection DuplicatedCode */
 Route::middleware('logged-user')->group(function()use ($url,$controllerClass){
     Route::match(['GET','POST'],$url.'/initialize', [$controllerClass, 'initialize']);
     Route::match(['GET','POST'],$url.'/get-items', [$controllerClass, 'getItems']);
-    Route::match(['GET','POST'],$url.'/get-item/{itemId}', [$controllerClass, 'getItem']);
-    Route::post($url.'/save-item', [$controllerClass, 'saveItem']);
 });
-
-
-$url='reports/varieties-yearly';
-$controllerClass= Controllers\reports\VarietiesYearlyController::class;
-/** @noinspection DuplicatedCode */
-Route::middleware('logged-user')->group(function()use ($url,$controllerClass){
-    Route::match(['GET','POST'],$url.'/initialize', [$controllerClass, 'initialize']);
-    Route::match(['GET','POST'],$url.'/get-items', [$controllerClass, 'getItems']);
-    Route::match(['GET','POST'],$url.'/get-item/{itemId}', [$controllerClass, 'getItem']);
-    Route::post($url.'/save-item', [$controllerClass, 'saveItem']);
-});
-
