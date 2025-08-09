fb1f8a36033c164d322e0d0b7442ed62b17de0f3
Shaiful Islam <islambuet@gmail.com>
2025-08-09 06:06:30 +0600

    sales vs target report completed

--- app/Http/Controllers/reports/route.api.php
+++ app/Http/Controllers/reports/route.api.php
@@ -24,3 +24,10 @@ Route::middleware('logged-user')->group(function()use ($url,$controllerClass){
     Route::match(['GET','POST'],$url.'/initialize', [$controllerClass, 'initialize']);
     Route::match(['GET','POST'],$url.'/get-items', [$controllerClass, 'getItems']);
 });
+$url='reports/sales_target';
+$controllerClass= Controllers\reports\SalesTargetReportController::class;
+/** @noinspection DuplicatedCode */
+Route::middleware('logged-user')->group(function()use ($url,$controllerClass){
+    Route::match(['GET','POST'],$url.'/initialize', [$controllerClass, 'initialize']);
+    Route::match(['GET','POST'],$url.'/get-items', [$controllerClass, 'getItems']);
+});
