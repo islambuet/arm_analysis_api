aee5e4e403aa6f5a9f56b365b33b19e98eb6a62c
Shaiful Islam <islambuet@gmail.com>
2025-11-19 15:03:21 +0600

    Update SalesTargetReportController.php

--- app/Http/Controllers/analysis_reports/SalesTargetReportController.php
+++ app/Http/Controllers/analysis_reports/SalesTargetReportController.php
@@ -54,8 +54,10 @@ class SalesTargetReportController extends RootController
                 ->select('id', 'name', 'status')
                 ->orderBy('ordering', 'ASC')
                 ->get();
-            $response['crop_types'] = DB::table(TABLE_CROP_TYPES)
-                ->select('id', 'name','crop_id', 'status')
+            $response['crop_types'] = DB::table(TABLE_CROP_TYPES.' as crop_types')
+                ->select('crop_types.*')
+                ->join(TABLE_CROPS.' as crops', 'crops.id', '=', 'crop_types.crop_id')
+                ->addSelect('crops.name as crop_name')
                 ->orderBy('ordering', 'ASC')
                 ->get();
             $response['varieties']=DB::table(TABLE_VARIETIES.' as varieties')
@@ -87,29 +89,50 @@ class SalesTargetReportController extends RootController
             $options = $request->input('options');
             $start_date=($options['fiscal_year']).'-'.ConfigurationHelper::getCurrentFiscalYearStartingMonth().'-01';
             $end_date=($options['fiscal_year']+1).'-'.ConfigurationHelper::getCurrentFiscalYearStartingMonth().'-01';
+            //varieties unit price setup for target start
+            $results = DB::table(TABLE_PACK_SIZES)
+                ->select('variety_id')
+                ->addSelect(DB::raw('AVG(unit_price_per_kg) as unit_price_per_kg'))
+                ->where('status', SYSTEM_STATUS_ACTIVE)
+                ->groupBy('variety_id')
+                ->get();
+            $varieties_unit_price_per_kg=[];
+            foreach ($results as $result){
+                $varieties_unit_price_per_kg[$result->variety_id]=round($result->unit_price_per_kg,2);
+            }
+
+            $query=DB::table(TABLE_DISTRIBUTORS_SALES.' as sd');
+            $query->join(TABLE_PACK_SIZES.' as ps', 'ps.id', '=', 'sd.pack_size_id');
+            $query->select(DB::raw('SUM(quantity) as quantity'),DB::raw('SUM(amount) as amount'));
+            $query->addSelect('ps.variety_id as variety_id');
+            $query->where('sd.sales_at','>=',$start_date);
+            $query->where('sd.sales_at','<',$end_date);
+            $query->groupBy('ps.variety_id');
+            $results=$query->get();
+            foreach ($results as $result){
+                if($result->quantity>0){//always true
+                    $varieties_unit_price_per_kg[$result->variety_id]=round($result->amount/$result->quantity,3);
+                }
+
+            }
+            //varieties unit price setup for target end
+
+            //sales start
+
             $query=DB::table(TABLE_DISTRIBUTORS_SALES.' as sd');
 
             $query->join(TABLE_PACK_SIZES.' as ps', 'ps.id', '=', 'sd.pack_size_id');
-            $query->join(TABLE_VARIETIES.' as varieties', 'varieties.id', '=', 'ps.variety_id');
-            $query->join(TABLE_CROP_TYPES.' as crop_types', 'crop_types.id', '=', 'varieties.crop_type_id');
             $query->join(TABLE_DISTRIBUTORS.' as d', 'd.id', '=', 'sd.distributor_id');
             $query->join(TABLE_LOCATION_TERRITORIES.' as territories', 'territories.id', '=', 'd.territory_id');
             $query->join(TABLE_LOCATION_AREAS.' as areas', 'areas.id', '=', 'territories.area_id');
 
             $query->select(DB::raw('SUM(quantity) as quantity'),DB::raw('SUM(amount) as amount'));
-            $query->addSelect('varieties.id as variety_id');
+            $query->addSelect('ps.variety_id as variety_id');
+            $query->addSelect('sd.distributor_id as distributor_id');
 
             $query->where('sd.sales_at','>=',$start_date);
             $query->where('sd.sales_at','<',$end_date);
-            if($options['crop_id']>0){
-                $query->where('crop_types.crop_id','=',$options['crop_id']);
-                if($options['crop_type_id']>0){
-                    $query->where('crop_types.id','=',$options['crop_type_id']);
-                    if($options['variety_id']>0){
-                        $query->where('varieties.id','=',$options['variety_id']);
-                    }
-                }
-            }
+
             if($options['part_id']>0){
                 $query->where('areas.part_id','=',$options['part_id']);
                 if($options['area_id']>0){
@@ -122,10 +145,29 @@ class SalesTargetReportController extends RootController
                     }
                 }
             }
-            $query->groupBy('varieties.id');
+            $query->groupBy('ps.variety_id');
+            $query->groupBy('sd.distributor_id');
 
             $results=$query->get();
-            $response['sales']=$results;
+
+            $response['sales_targets']=[];
+            foreach ($results as $result){
+                if(!isset($response['sales_targets'][$result->variety_id])){
+                    $response['sales_targets'][$result->variety_id]=[];
+                    $response['sales_targets'][$result->variety_id]['unit_price']=0;
+                    if(isset($varieties_unit_price_per_kg[$result->variety_id])){
+                        $response['sales_targets'][$result->variety_id]['unit_price']=$varieties_unit_price_per_kg[$result->variety_id];
+                    }
+                    $response['sales_targets'][$result->variety_id]['distributors']=[];
+                }
+                $response['sales_targets'][$result->variety_id]['distributors'][$result->distributor_id]=[];
+                $response['sales_targets'][$result->variety_id]['distributors'][$result->distributor_id]['quantity_target']=0;
+                $response['sales_targets'][$result->variety_id]['distributors'][$result->distributor_id]['amount_target']=0;
+                $response['sales_targets'][$result->variety_id]['distributors'][$result->distributor_id]['quantity_sales']=$result->quantity;
+                $response['sales_targets'][$result->variety_id]['distributors'][$result->distributor_id]['amount_sales']=$result->amount;
+            }
+
+            //sales end
 
             //Target fiscal year start
 
@@ -152,18 +194,32 @@ class SalesTargetReportController extends RootController
             }
 
             $results=$query->get();
-            $response['target']=[];
+
             foreach ($results as $result){
                 if($result){
                     if($result->varieties){
-                        $stock=json_decode($result->varieties);
-                        foreach ($stock as $variety_id=>$quantity){
+                        $varieties=json_decode($result->varieties);
+                        foreach ($varieties as $variety_id=>$quantity){
                             if(is_numeric($quantity)){
-                                if(isset($response['target'][$variety_id])){
-                                    $response['target'][$variety_id]+=$quantity;
+                                if(!isset($response['sales_targets'][$variety_id])){
+                                    $response['sales_targets'][$variety_id]=[];
+                                    $response['sales_targets'][$variety_id]['unit_price']=0;
+                                    if(isset($varieties_unit_price_per_kg[$variety_id])){
+                                        $response['sales_targets'][$variety_id]['unit_price']=$varieties_unit_price_per_kg[$variety_id];
+                                    }
+                                    $response['sales_targets'][$variety_id]['distributors']=[];
+                                }
+                                if(isset($response['sales_targets'][$variety_id]['distributors'][$result->distributor_id]))
+                                {
+                                    $response['sales_targets'][$variety_id]['distributors'][$result->distributor_id]['quantity_target']=$quantity;
+                                    $response['sales_targets'][$variety_id]['distributors'][$result->distributor_id]['amount_target']=$quantity*$response['sales_targets'][$variety_id]['unit_price'];
                                 }
                                 else{
-                                    $response['target'][$variety_id]=$quantity;
+                                    $response['sales_targets'][$variety_id]['distributors'][$result->distributor_id]=[];
+                                    $response['sales_targets'][$variety_id]['distributors'][$result->distributor_id]['quantity_target']=$quantity;
+                                    $response['sales_targets'][$variety_id]['distributors'][$result->distributor_id]['amount_target']=$quantity*$response['sales_targets'][$variety_id]['unit_price'];
+                                    $response['sales_targets'][$variety_id]['distributors'][$result->distributor_id]['quantity_sales']=0;
+                                    $response['sales_targets'][$variety_id]['distributors'][$result->distributor_id]['amount_sales']=0;
                                 }
                             }
                         }
